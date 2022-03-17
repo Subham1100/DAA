f4345855f853f83769b244495a6f20faf6de7327
subham <subhamchauhan1100@gmail.com>
2022-03-17 11:49:52 +0530

    Initial Commit

---
+++ DAA/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  DAA
+//
+//  Created by subham chauhan on 17/03/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
